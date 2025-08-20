7f0b62d38babb1f1086fe8fceb012855b9132ff4
SAM42 <144408939+Sam42a@users.noreply.github.com>
2025-08-20 16:21:05 +0200

    Remove logo from DreamContentLogo composable
    The ScreenSaver  no longer displays the app logo and now uses a transparent background instead of black. This simplifies the UI by removing the logo image and improves performance.

--- app/src/main/java/org/jellyfin/androidtv/integration/dream/composable/DreamContentLogo.kt
+++ app/src/main/java/org/jellyfin/androidtv/integration/dream/composable/DreamContentLogo.kt
@@ -3,13 +3,13 @@ package org.jellyfin.androidtv.integration.dream.composable
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -19,14 +19,7 @@ import org.jellyfin.androidtv.R
 fun DreamContentLogo() = Box(
 	modifier = Modifier
 		.fillMaxSize()
-		.background(Color.Black),
+		.background(Color.Transparent),
 ) {
-	Image(
-		painter = painterResource(R.drawable.app_logo),
-		contentDescription = stringResource(R.string.app_name),
-		modifier = Modifier
-			.align(Alignment.Center)
-			.width(400.dp)
-			.fillMaxHeight()
-	)
+	// Logo removed
 }
