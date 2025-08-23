32e389188d334598cca714c2cefa3c3cd48894cb
SAM42 <144408939+Sam42a@users.noreply.github.com>
2025-08-23 15:05:45 +0200

    Add missing ItemField

--- app/src/main/java/org/jellyfin/androidtv/data/repository/ItemRepository.kt
+++ app/src/main/java/org/jellyfin/androidtv/data/repository/ItemRepository.kt
@@ -1,37 +1,31 @@
 package org.jellyfin.androidtv.data.repository
-
 import org.jellyfin.sdk.model.api.ItemFields
+import timber.log.Timber
 
+// Repository defining item fields for Jellyfin API queries
 object ItemRepository {
+	init {
+		Timber.d("Initializing ItemRepository with %d item fields", itemFields.size)
+	}
+
 	val itemFields = setOf(
-		// Item metadata
-		ItemFields.OVERVIEW,
-		ItemFields.GENRES,
-		ItemFields.TAGLINES,
-		ItemFields.DATE_CREATED,
-		ItemFields.PATH,
-
-		// Media information
-		ItemFields.MEDIA_SOURCES,
-		ItemFields.MEDIA_SOURCE_COUNT,
-		ItemFields.MEDIA_STREAMS,
+		ItemFields.CAN_DELETE,
+		ItemFields.CHANNEL_INFO,
 		ItemFields.CHAPTERS,
-		ItemFields.TRICKPLAY,
-
-		// Image and display
+		ItemFields.CHILD_COUNT,
+		ItemFields.CUMULATIVE_RUN_TIME_TICKS,
+		ItemFields.DATE_CREATED,
+		ItemFields.DISPLAY_PREFERENCES_ID,
+		ItemFields.GENRES,
+		ItemFields.ITEM_COUNTS,
+		ItemFields.MEDIA_SOURCE_COUNT,
+		ItemFields.MEDIA_SOURCES,
+		ItemFields.MEDIA_STREAMS,
+		ItemFields.OVERVIEW,
+		ItemFields.PATH,
 		ItemFields.PRIMARY_IMAGE_ASPECT_RATIO,
 		ItemFields.SERIES_PRIMARY_IMAGE,
-		ItemFields.DISPLAY_PREFERENCES_ID,
-
-		// Item counts and properties
-		ItemFields.CHILD_COUNT,
-		ItemFields.ITEM_COUNTS,
-		ItemFields.CUMULATIVE_RUN_TIME_TICKS,
-
-		// Permissions
-		ItemFields.CAN_DELETE,
-
-		// Channel information
-		ItemFields.CHANNEL_INFO
+		ItemFields.TAGLINES,
+		ItemFields.TRICKPLAY,
 	)
 }
